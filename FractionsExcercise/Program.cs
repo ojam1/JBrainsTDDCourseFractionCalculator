ebcd3b7ad7dd322f3ec89fe00bb6a9a88ec492d8
Oliver Ranson <Oliver.Ranson@firsthelpline.com>
2018-06-27 10:09:49 +0100

    Clears program class

--- FractionsExcercise/Program.cs
+++ FractionsExcercise/Program.cs
@@ -2,11 +2,8 @@
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var fraction = new Fraction(16, 4);
-            fraction.Print();
-            FractionCalculator.Add(new Fraction(1231243, 44545), new Fraction(3232, 3344)).Print();
         }
     }
 }
